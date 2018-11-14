03d976ecba5dbaaac141be265321d56407867b45
HDuck <1ntranc3d@gmail.com>
2018-11-14 21:00:15 +0300

    Added departure flights request and delayed flights handler (get and render delayed table)

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@
     const APP_ID = '7f1e1273';
     const AIRPORT_CODE = 'SVO';
     const TABLE_CLASS = 'avia-board';
+    const DELAY_THRESHOLD = 15;
 
     class TableRow {
-        constructor(date, city, fscode, flightNum, terminal, status) {
+        constructor(date, city, fscode, flightNum, terminal, status, delayed, delayedDate, delay, direction) {
             this.date = date;
             this.city = city;
             this.fsCode = fscode;
             this.fNumber = flightNum;
             this.terminal = terminal;
             this.status = status;
+            this.delayed = delayed;
+            this.delayedDate = delayedDate;
+            this.delay = delay;
+            this.direction = direction;
         }
 
         render() {
@@ -20,12 +25,16 @@
 
             let time = document.createElement('span');
             time.className = 'col-1 time';
-            time.textContent = this.date;
+            time.textContent = this.delayed
+                ? `${this.delayedDate} (${this.delay}min)`
+                : this.date;
             newRow.appendChild(time);
 
             let city = document.createElement('span');
             city.className = 'col-4 city';
-            city.textContent = this.city;
+            city.textContent = this.delayed && this.direction
+                ? `${this.city} (${this.direction})`
+                : this.city;
             newRow.appendChild(city);
 
             let flightNums = document.createElement('div');
@@ -58,34 +67,103 @@
     let today = getCurrDate();
     let direction = 'arr';
     let flightsApi = `https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/airport/status/${AIRPORT_CODE}/${direction}/${today}?appId=${APP_ID}&appKey=${APP_KEY}&utc=false&numHours=2`;
+    let flightsDummyUrl = 'https://api.flightstats.com/';
 
-    let flightData;
+    let resultsArr = [];
 
     $.ajax({
         method: 'GET',
-        url: 'https://api.flightstats.com/',
+        url: flightsApi,
         dataType: 'jsonp',
-        jsonpCallback: 'callback',
-        success: (res) => {
-            console.log(`Request - success!\nGetting data from external API..`);
-            flightData = res;
-        },
-        error: (_, status) => {
-            console.log(`Request - failed! Error: ${status}\nGetting data from local source..`);
-            flightData = JSON.parse(testJsonObj);
-        }
+        jsonpCallback: 'callback'
     })
-    .done(() => {
-        let flights = getData(flightData, direction);
-        renderTable(flights, direction);
-    })
-    .fail(() => {
-        let flights = getData(flightData, direction);
-        renderTable(flights, direction);
-    });
+        .done((arrivalData) => {
+            console.log(`Request for arrival flights have been completed with success!\nGetting data from external API..`);
+
+            let arrFlights = getData(arrivalData, direction);
+            renderTable(arrFlights, direction);
+            resultsArr.push(arrFlights);
+
+            // Второй последовательный запрос 
+
+            direction = 'dep';
+            let flightsApi = `https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/airport/status/${AIRPORT_CODE}/${direction}/${today}?appId=${APP_ID}&appKey=${APP_KEY}&utc=false&numHours=2`;
+
+            return $.ajax({
+                method: 'GET',
+                url: flightsApi,
+                dataType: 'jsonp',
+                jsonpCallback: 'callback'
+            })
+                .done((departureData) => {
+                    console.log(`Request for departure flights have been completed with success!\nGetting data from external API..`);
+    
+                    let depFlights = getData(departureData, direction);
+                    renderTable(depFlights, direction);
+                    resultsArr.push(depFlights);
+
+                    let delFlights = getDelayedFlights(resultsArr);
+                    renderTable(delFlights);
+                })
+                .fail((_, status) => {
+                    console.log(`Request for departure flights have been failed! Error: ${status}\nGetting data from local source..`);
+    
+                    let data = JSON.parse(testJsonObj);
+    
+                    let depFlights = getData(data, direction);
+                    renderTable(depFlights, direction);
+                    resultsArr.push(depFlights);
+
+                    let delFlights = getDelayedFlights(resultsArr);
+                    renderTable(delFlights);
+                });
+        })
+        .fail((_, status) => {
+            console.log(`Request for arrival flights have been failed! Error: ${status}\nGetting data from local source..`);
+
+            let data = JSON.parse(testJsonObj);
+
+            let arrFlights = getData(data, direction);
+            renderTable(arrFlights, direction);
+            resultsArr.push(arrFlights);
+            
+            // Второй последовательный запрос 
+
+            direction = 'dep';
+            let flightsApi = `https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/airport/status/${AIRPORT_CODE}/${direction}/${today}?appId=${APP_ID}&appKey=${APP_KEY}&utc=false&numHours=2`;
+
+            return $.ajax({
+                method: 'GET',
+                url: flightsApi,
+                dataType: 'jsonp',
+                jsonpCallback: 'callback'
+            })
+                .done((departureData) => {
+                    console.log(`Request for departure flights have been completed with success!\nGetting data from external API..`);
+    
+                    let depFlights = getData(departureData, direction);
+                    renderTable(depFlights, direction);
+                    resultsArr.push(depFlights);
+
+                    let delFlights = getDelayedFlights(resultsArr);
+                    renderTable(delFlights);
+                })
+                .fail((_, status) => {
+                    console.log(`Request for departure flights have been failed! Error: ${status}\nGetting data from local source..`);
+    
+                    let data = JSON.parse(testJsonObj);
+    
+                    let depFlights = getData(data, direction);
+                    renderTable(depFlights, direction);
+                    resultsArr.push(depFlights);
+
+                    let delFlights = getDelayedFlights(resultsArr);
+                    renderTable(delFlights);
+                });
+        });
 
     // Создание массива с необходимыми данными
-
+    
     function getData(data, direction) {
         let flightsArr = [];
 
@@ -96,13 +174,17 @@
                 fsCode: [],
                 flightNum: [],
                 terminal: '',
-                status: ''
+                status: '',
+                delayed: false,
+                delayedDate: '',
+                delay: '',
+                direction: ''
             };
 
             flight.fsCode.push(fStatus.carrierFsCode);
             flight.flightNum.push(fStatus.flightNumber);
 
-            if (fStatus.codeshares) {
+            if (checkCodeshares(fStatus)) {
 
                 fStatus.codeshares.forEach((item) => {
                     flight.fsCode.push(item.fsCode);
@@ -148,6 +230,10 @@
                 }
             }
 
+            flight.direction = direction === 'arr'
+                ? 'arrival'
+                : 'departure';
+
             if (direction === 'arr') {
                 flight.date = fStatus.arrivalDate.dateLocal.match(/\d\d:\d\d/)[0];
                 
@@ -159,8 +245,27 @@
                     }
                 });
 
-                if (fStatus.airportResources && fStatus.airportResources.arrivalTerminal) {
+                if (checkAirportResources(fStatus, direction)) {
                     flight.terminal = fStatus.airportResources.arrivalTerminal;
+                }
+
+                if (checkOperationalTimes(fStatus, direction)) {
+                    let scheduledGArrTime = fStatus.operationalTimes.scheduledGateArrival.dateLocal.match(/(\d\d):(\d\d)/)[0];
+                    let estimatedGArrTime = fStatus.operationalTimes.estimatedGateArrival.dateLocal.match(/(\d\d):(\d\d)/)[0];
+
+                    let scheduledTimeHours = parseInt(scheduledGArrTime.replace(/(\d\d):(\d\d)/, '$1'), 10);
+                    let scheduledTimeMinutes = parseInt(scheduledGArrTime.replace(/(\d\d):(\d\d)/, '$2'), 10);
+                    let estimatedTimeHours = parseInt(estimatedGArrTime.replace(/(\d\d):(\d\d)/, '$1'), 10);
+                    let estimatedTimeMinutes = parseInt(estimatedGArrTime.replace(/(\d\d):(\d\d)/, '$2'), 10);
+
+                    let difference = estimatedTimeHours * 60 + estimatedTimeMinutes - (scheduledTimeHours * 60 + scheduledTimeMinutes);
+
+                    if (difference >= DELAY_THRESHOLD) {
+
+                        flight.delayed = true;
+                        flight.delayedDate = estimatedGArrTime;
+                        flight.delay = difference;
+                    }
                 }
 
             } else if (direction === 'dep') {
@@ -174,16 +279,83 @@
                     }
                 });
 
-                if (fStatus.airportResources && fStatus.airportResources.departureTerminal) {
+                if (checkAirportResources(fStatus, direction)) {
                     flight.terminal = fStatus.airportResources.departureTerminal;
+                }
+                
+                if (checkOperationalTimes(fStatus, direction)) {
+                    let scheduledGDepTime = fStatus.operationalTimes.scheduledGateDeparture.dateLocal.match(/(\d\d):(\d\d)/)[0];
+                    let estimatedGDepTime = fStatus.operationalTimes.estimatedGateDeparture.dateLocal.match(/(\d\d):(\d\d)/)[0];
+
+                    let scheduledTimeHours = parseInt(scheduledGDepTime.replace(/(\d\d):(\d\d)/, '$1'), 10);
+                    let scheduledTimeMinutes = parseInt(scheduledGDepTime.replace(/(\d\d):(\d\d)/, '$2'), 10);
+                    let estimatedTimeHours = parseInt(estimatedGDepTime.replace(/(\d\d):(\d\d)/, '$1'), 10);
+                    let estimatedTimeMinutes = parseInt(estimatedGDepTime.replace(/(\d\d):(\d\d)/, '$2'), 10);
+
+                    let difference = estimatedTimeHours * 60 + estimatedTimeMinutes - (scheduledTimeHours * 60 + scheduledTimeMinutes);
+
+                    if (difference >= DELAY_THRESHOLD) {
+
+                        flight.delayed = true;
+                        flight.delayedDate = estimatedGDepTime;
+                        flight.delay = difference;
+                    }
                 }
             }
 
             flightsArr.push(flight);
         });
 
+        function checkCodeshares(fStatus) {
+            return fStatus.codeshares;
+        }
+
+        function checkAirportResources(fStatus, direction) {
+            
+            return direction === 'arr'
+                ? fStatus.airportResources
+                    && fStatus.airportResources.arrivalTerminal
+                : fStatus.airportResources
+                    && fStatus.airportResources.departureTerminal;
+        }
+
+        function checkOperationalTimes(fStatus, direction) {
+            
+            return direction === 'arr'
+                ? fStatus.operationalTimes
+                    && fStatus.operationalTimes.scheduledGateArrival
+                    && fStatus.operationalTimes.estimatedGateArrival
+                : fStatus.operationalTimes
+                    && fStatus.operationalTimes.scheduledGateDeparture
+                    && fStatus.operationalTimes.estimatedGateDeparture;
+        }
+
         return flightsArr;
     };
+
+    function getDelayedFlights(results) {
+        let delFlightsArr = [];
+
+        // Для прилетающих рейсов
+
+        results[0].forEach((flight) => {
+
+            if (flight.delayed) {
+                delFlightsArr.push(flight);
+            }
+        })
+
+        // Для улетающих рейсов
+
+        results[1].forEach((flight) => {
+
+            if (flight.delayed) {
+                delFlightsArr.push(flight);
+            }
+        });
+
+        return delFlightsArr;
+    }
 
     // Отрисовка таблицы с данными рейсов
 
@@ -196,18 +368,43 @@
 
         let tableCont = document.querySelector(selector);
 
-        dataArr.forEach((item) => {
-            let row = new TableRow(
-                item.date,
-                item.city,
-                item.fsCode,
-                item.flightNum,
-                item.terminal,
-                item.status
-            );
-            
-            tableCont.appendChild(row.render());
-        });
+        if (direction) {
+            dataArr.forEach((item) => {
+                let row = new TableRow(
+                    item.date,
+                    item.city,
+                    item.fsCode,
+                    item.flightNum,
+                    item.terminal,
+                    item.status,
+                    item.delayed,
+                    item.delayedDate,
+                    item.delay
+                );
+                
+                tableCont.appendChild(row.render());
+            });
+        
+        } else {
+            dataArr.forEach((item) => {
+                let row = new TableRow(
+                    item.date,
+                    item.city,
+                    item.fsCode,
+                    item.flightNum,
+                    item.terminal,
+                    item.status,
+                    item.delayed,
+                    item.delayedDate,
+                    item.delay,
+                    item.direction
+                );
+                
+                tableCont.appendChild(row.render());
+            });
+        }
+
+        
     }
 
     // Текущая дата для запроса на API
